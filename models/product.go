c9e0dcbd4d79ac80610c5518bc60db034c4723ad
Adiansyah <mail.adiansyah@gmail.com>
2022-07-31 06:27:05 +0700

    fix: change table structure for product table

--- models/product.go
+++ models/product.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Product struct {
-	ID         string    `json:"id" xml:"id" form:"id" query:"id" gorm:"type:varchar; primary_key"`
-	Name       string    `json:"name" xml:"name" form:"name" query:"name" gorm:"type:varchar; not null"`
-	SKU        uint64    `json:"sku" xml:"sku" form:"sku" query:"sku" gorm:"type:bigint; index"`
-	CategoryId string    `json:"category_id" xml:"category_id" form:"category_id" query:"category_id" gorm:"type:varchar; index"`
-	CreatedAt  time.Time `json:"created_at" xml:"created_at" query:"created_at"`
-	UpdatedAt  time.Time `json:"updated_at" xml:"updated_at" query:"updated_at"`
+	ID         string    `json:"id" xml:"id" form:"id" query:"id" gorm:"primaryKey;type:varchar(255);NOT NULL"`
+	Name       string    `json:"name" xml:"name" form:"name" query:"name" gorm:"type:varchar(255);NOT NULL"`
+	SKU        uint64    `json:"sku" xml:"sku" form:"sku" query:"sku" gorm:"type:varchar(255)"`
+	CategoryId string    `json:"category_id" xml:"category_id" form:"category_id" query:"category_id" gorm:"type:varchar(255);NOT NULL;index"`
+	CreatedAt  time.Time `json:"created_at" xml:"created_at" query:"created_at" gorm:"NOT NULL;default:CURRENT_TIMESTAMP"`
+	UpdatedAt  time.Time `json:"updated_at" xml:"updated_at" query:"updated_at" gorm:"NOT NULL;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
 	DeletedAt  time.Time `json:"deleted_at" xml:"deleted_at" query:"deleted_at"`
-	CreatedBy  string    `json:"created_by" xml:"created_by" query:"created_by"`
-	UpdatedBy  string    `json:"updated_by" xml:"updated_by" query:"updated_by"`
-	DeletedBy  string    `json:"deleted_by" xml:"deleted_by" query:"deleted_by"`
+	CreatedBy  string    `json:"created_by" xml:"created_by" query:"created_by" gorm:"type:varchar(255)"`
+	UpdatedBy  string    `json:"updated_by" xml:"updated_by" query:"updated_by" gorm:"type:varchar(255)"`
+	DeletedBy  string    `json:"deleted_by" xml:"deleted_by" query:"deleted_by" gorm:"type:varchar(255)"`
 }
