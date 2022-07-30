ec5ce9b1823c26d750d74f42e57a126b521c971f
Adiansyah <mail.adiansyah@gmail.com>
2022-07-30 14:23:48 +0700

    achive: init product schema

---
+++ schemas/product.go
@@ -0,0 +1,8 @@
+package schemas
+
+type SchemaProduct struct {
+	ID         string `json:"id" validate:"uuid"`
+	Name       string `json:"name" validate:"required,lowercase"`
+	SKU        uint64 `json:"sku" validate:"required,numeric"`
+	CategoryId uint64 `json:"category_id" validate:"required,uuid"`
+}
