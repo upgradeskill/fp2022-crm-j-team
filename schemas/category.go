3bc2a532cf49ce6c467b91a18dcddd82ac7d32b3
abdullahaaf1 <abdullah.amin@majoo.id>
2022-07-31 18:48:23 +0700

    add category schedma

---
+++ schemas/category.go
@@ -0,0 +1,7 @@
+package schemas
+
+type Category struct {
+	ID          string `json:"id" validate:"uuid"`
+	Name        string `json:"name" validate:"required,lowercase"`
+	Description string `json:"description"`
+}
