09be9e8cb059d51cef1fe26205f64249d1a7d2f1
Adiansyah <mail.adiansyah@gmail.com>
2022-08-03 06:11:08 +0700

    breaking change! product now implement ApiResponse helper

--- handlers/product.go
+++ handlers/product.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 
+	"github.com/upgradeskill/fp2022-crm-j-team/helpers"
 	"github.com/upgradeskill/fp2022-crm-j-team/ports"
 	"github.com/upgradeskill/fp2022-crm-j-team/schemas"
 )
@@ -26,28 +27,44 @@ func Product(product ports.Product) *handleProduct {
 
 func (h *handleProduct) Create(c echo.Context) error {
 	product := new(schemas.Product)
+
 	if err := c.Bind(product); err != nil {
-		return c.JSON(http.StatusBadRequest, "create failed")
+		helpers.APIResponse(c, err.Error(), http.StatusBadRequest, nil)
+		return nil
 	}
 
 	_, err := h.product.Create(product)
 	if err.Code != 0 {
-		return c.JSON(http.StatusBadRequest, "create failed")
+		helpers.APIResponse(c, "Failed to create product", err.Code, nil)
+		return nil
 	}
 
-	return c.JSON(http.StatusCreated, "success")
+	helpers.APIResponse(c, "Success", http.StatusCreated, nil)
+	return nil
 }
 
 func (h *handleProduct) Get(c echo.Context) error {
 	product := new(schemas.Product)
 	product.ID = c.Param("id")
 
-	res, err := h.product.Get(product)
-	if err.Code != 0 {
-		return c.JSON(http.StatusBadRequest, "get data failed")
+	if product.ID == "" {
+		helpers.APIResponse(c, "Id is required", http.StatusBadRequest, nil)
+		return nil
 	}
 
-	return c.JSON(http.StatusCreated, res)
+	res, err := h.product.Get(product)
+	if err.Code != 0 {
+		helpers.APIResponse(c, "Failed to get product", err.Code, nil)
+		return nil
+	}
+
+	if res == nil {
+		helpers.APIResponse(c, "Product not found", http.StatusNotFound, nil)
+		return nil
+	}
+
+	helpers.APIResponse(c, "Success", http.StatusOK, res)
+	return nil
 }
 
 func (h *handleProduct) GetAll(c echo.Context) error {
@@ -61,10 +78,12 @@ func (h *handleProduct) GetAll(c echo.Context) error {
 
 	res, err := h.product.GetAll(product)
 	if err.Code != 0 {
-		return c.JSON(http.StatusBadRequest, "get data failed")
+		helpers.APIResponse(c, "Failed to get products", err.Code, nil)
+		return nil
 	}
 
-	return c.JSON(http.StatusCreated, res)
+	helpers.APIResponse(c, "Success", http.StatusOK, res)
+	return nil
 }
 
 func (h *handleProduct) Update(c echo.Context) error {
@@ -74,23 +93,35 @@ func (h *handleProduct) Update(c echo.Context) error {
 	}
 
 	product.ID = c.Param("id")
+	if product.ID == "" {
+		helpers.APIResponse(c, "Id is required", http.StatusBadRequest, nil)
+		return nil
+	}
 
 	_, err := h.product.Update(product)
 	if err.Code != 0 {
-		return c.JSON(http.StatusBadRequest, "update failed")
+		helpers.APIResponse(c, "Failed to get products", err.Code, nil)
+		return nil
 	}
 
-	return c.JSON(http.StatusCreated, "success")
+	helpers.APIResponse(c, "Success", http.StatusOK, nil)
+	return nil
 }
 
 func (h *handleProduct) Delete(c echo.Context) error {
 	product := new(schemas.Product)
 	product.ID = c.Param("id")
+	if product.ID == "" {
+		helpers.APIResponse(c, "Id is required", http.StatusBadRequest, nil)
+		return nil
+	}
 
 	_, err := h.product.Delete(product)
 	if err.Code != 0 {
-		return c.JSON(http.StatusBadRequest, "delete failed")
+		helpers.APIResponse(c, "Failed to get products", err.Code, nil)
+		return nil
 	}
 
-	return c.JSON(http.StatusCreated, "success")
+	helpers.APIResponse(c, "Success", http.StatusOK, nil)
+	return nil
 }
